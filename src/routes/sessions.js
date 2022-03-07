1ee1caacfea75dca510b54907f6e608af6fdc0b2
Norton <38260864+nortonglitz@users.noreply.github.com>
2022-03-07 00:06:35 -0300

    0.0.2 js free

--- src/routes/sessions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var auth_1 = require("middlewares/auth");
-var sessions_1 = require("controllers/sessions");
-var router = (0, express_1.Router)();
-router.post('/:userId', auth_1.checkCredentials, sessions_1.createSession);
-exports["default"] = router;
--- src/routes/users.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var users_1 = require("controllers/users");
-var router = (0, express_1.Router)();
-router.post('/', users_1.createUser);
-router.put('/:userId/:field', users_1.editUser);
-exports["default"] = router;
